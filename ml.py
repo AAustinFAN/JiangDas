dfb339729004793d253028b2f651ad55512f7829
Administrator <18135966578@163.com>
2021-08-07 13:45:52 +0800

    ml update

--- ml.py
+++ ml.py
@@ -12,15 +12,16 @@ for x in range(train_data.shape[0]):
         train_unknown_list.append(x)
 
 train_data = np.delete(train_data, train_unknown_list, axis=0)
-X_train = train_data[:, (6, 5, 3)].astype(float)
-y_train = train_data[:, -1].astype(float)
+X_train = train_data[:100, (6, 5, 3)]
+y_train = train_data[:100, -1]
 
 df = pd.read_csv('all_chiller.csv')
 df = df.drop(labels=417)
 #df = df.drop(labels=30)
 test_data = df.drop(range(0, 30))
-X_test = np.array(test_data[['冷水机组冷冻供水温度', '冷水机组冷冻回水温度', '冷水机组负荷比']]).astype(float)
-y_test = np.array(test_data['cop']).astype(float)
+X_test = np.array(test_data[['冷水机组冷冻供水温度', '冷水机组冷冻回水温度', '冷水机组负荷比']])[:2000]
+y_test = np.array(test_data['cop'])[:2000]
+
 
 ss_X=StandardScaler()
 ss_y=StandardScaler()
@@ -30,10 +31,9 @@ X_test=ss_X.transform(X_test)
 y_train=ss_y.fit_transform(y_train.reshape(-1, 1))
 y_test=ss_y.transform(y_test.reshape(-1, 1))
 
-poly_svr=SVR(kernel='poly')   #多项式核函数初始化的SVR
+poly_svr=SVR(kernel='linear')   #多项式核函数初始化的SVR
 poly_svr.fit(X_train,y_train)
 poly_svr_y_predict=poly_svr.predict(X_test)
-
 
 print(' ')
 print('R-squared value of Poly SVR is',poly_svr.score(X_test,y_test))
